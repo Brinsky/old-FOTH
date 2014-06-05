b961df20669c108f83e415fc95d55ede6b995b64
Brian (Brinsky) <Brinsky@users.noreply.github.com>
2014-06-04 21:52:12 -0400

    Fixed accidental <iostream> include

--- src/FOTHgrid.cpp
+++ src/FOTHgrid.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "FOTHgrid.h"
 
 FOTHgrid::FOTHgrid(int a_length, sf::Texture* a_FOTHdefaultTexture) : Grid(0, (10 - a_length) * 64, 7, a_length, 64, 64, a_FOTHdefaultTexture) {
