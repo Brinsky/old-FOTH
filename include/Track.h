1c1797ec7d7e24378bd11a2efeb205685f5c4c3c
Killian <else42@users.noreply.github.com>
2014-06-16 11:49:52 -0400

    Adding initial track files

---
+++ include/Track.h
@@ -0,0 +1,20 @@
+#ifndef TRACK
+#define TRACK
+
+#include "FOTHgridObject.h"
+#include <vector>
+
+class Track : public FOTHgridObject {
+
+public:
+	Track(sf::Texture* a_trackGraphic, GridSpace* a_parentGridSpace);
+	void interactWithTrain(/*train*/);
+	void draw(sf::RenderTarget& a_target, sf::RenderStates a_states);
+
+private:
+	static std::vector<sf::Vector2i> trackGridSpaces; 
+	bool isNeighborTrack(int a_xGridPosOffset, int a_yGridPosOffset);
+
+};
+
+#endif
---
+++ src/Track.cpp
@@ -0,0 +1,72 @@
+#include "Track.h"
+
+std::vector<sf::Vector2i> Track::trackGridSpaces;
+
+Track::Track(sf::Texture* a_trackGraphic, GridSpace* a_parentGridSpace) : FOTHgridObject(0, 0, a_trackGraphic, a_parentGridSpace){
+
+	trackGridSpaces.push_back(parentGridSpace->getGridSpacePosition());
+
+}
+
+bool Track::isNeighborTrack(int a_xGridPosOffset, int a_yGridPosOffset){
+
+	for(int i = 0; i < trackGridSpaces.size(); i++){
+
+		if(trackGridSpaces[i] == sf::Vector2i(parentGridSpace->getGridSpacePosition().x + a_xGridPosOffset, parentGridSpace->getGridSpacePosition().y + a_yGridPosOffset)){
+		
+			return true;
+
+		}
+
+	}
+
+	return false;
+
+}
+
+void Track::interactWithTrain(/*train*/){
+}
+
+void Track::draw(sf::RenderTarget& a_target, sf::RenderStates a_states){
+
+	objectGraphic.setPosition(parentGridSpace->getGridSpaceDimensions().x * (parentGridSpace->getGridSpacePosition().x + objectPosition.x), parentGridSpace->getGridSpaceDimensions().y * (parentGridSpace->getGridSpacePosition().y + objectPosition.y));
+
+	if(isNeighborTrack(0, -1)){
+
+		if(isNeighborTrack(1, 0)){
+		
+			objectGraphic.setTextureRect(sf::IntRect(0, 64, 64, 64));
+
+		}else if(isNeighborTrack(-1, 0)){
+			
+			objectGraphic.setTextureRect(sf::IntRect(64, 64, 64, 64));
+
+		}else{
+			
+			objectGraphic.setTextureRect(sf::IntRect(0, 0, 64, 64));
+
+		}
+		
+	}else if(isNeighborTrack(0, 1)){
+
+		if(isNeighborTrack(1, 0)){
+
+			objectGraphic.setTextureRect(sf::IntRect(0, 128, 64, 64));
+		
+		}else if(isNeighborTrack(-1, 0)){
+
+			objectGraphic.setTextureRect(sf::IntRect(64, 0, 64, 64));
+
+		}else{
+
+			objectGraphic.setTextureRect(sf::IntRect(0, 0, 64, 64));
+
+		}
+
+	}else if(isNeighborTrack(-1, 0) || isNeighborTrack(1, 0)){
+
+		objectGraphic.setTextureRect(sf::IntRect(64, 128, 64, 64));
+	
+	}
+
+}
